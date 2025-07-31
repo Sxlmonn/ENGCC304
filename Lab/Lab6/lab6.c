50d843294bc76b518f4440642b68ccae25342209
sxlmonn <ayakamistt@gmail.com>
2025-07-31 11:51:43 +0700

    if elesโหดๆ

---
+++ Lab/Lab6/lab6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    printf("Enter raw score: ");
+    scanf("%d", &N);
+
+    if (N % 2 == 1)
+    {
+        printf("%d:\n", N);
+        for (int i = 1; i <= N; i += 2)
+        {
+            printf("%d ", i);
+        }
+    }
+    else
+    {
+        printf("%d:\n", N);
+        for (int i = N; i >= 0; i -= 2)
+        {
+            printf("%d ", i);
+        }
+    }
+}
