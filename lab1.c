719f97fdaa38f051e77fc1aef43e6b575a89f382
sxlmonn <ayakamistt@gmail.com>
2025-10-10 23:27:15 +0700

    homework

--- lab1.c
+++ lab1.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    printf("Answer = %d", a + b + c);
+    printf("Answer = %d\n", a + b + c);
     return 0;
 }
